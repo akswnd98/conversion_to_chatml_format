f83716a4e6febc06b9a2b4d4515d47c233696d0e
akswnd98 <akswnd98@gmail.com>
2024-07-13 02:23:32 +0900

    first

--- generate_dataset_from_xz56_react_llama_format.py
+++ generate_dataset_from_xz56_react_llama_format.py
@@ -4,10 +4,17 @@ import json
 
 if __name__ == '__main__':
   original_dataset = load_dataset('xz56/react-llama')
+
+  excluded_indices = [651]
+  selected_indices = [i for i in range(len(original_dataset['train']))]
+  for excluded_index in excluded_indices:
+    selected_indices.remove(excluded_index)
+  selected_dataset = original_dataset['train'].select(selected_indices)
+
   converter = ReactLlamaToChatML()
 
   dataset_json = {
-    'train': [{'messages': converter.convert({'question': item['question'], 'trajectory': item['trajectory']})} for item in original_dataset['train']]
+    'train': [{'messages': converter.convert({'question': item['question'], 'trajectory': item['trajectory']})} for item in selected_dataset]
   }
 
   with open('dataset.json', 'w') as f:
